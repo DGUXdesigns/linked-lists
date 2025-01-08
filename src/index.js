827c97b15e0d88a8a333861bf3038ff9844ee956
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-08 04:06:24 -0500

    Add sample usage for LinkedList methods
    - Created an instance of the LinkedList class
    - Added various nodes to the list using append and prepend methods
    - Printed the list and accessed specific nodes using toString, at, and getSize methods
    - Demonstrated the removeAt method to remove a node from the list
    - Used console.table and console.log for displaying the list state at different stages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import './style.css';
+import { LinkedList } from './nodeLogic.js';
+
+const list = new LinkedList();
+
+list.append('dog');
+list.append('cat');
+list.append('parrot');
+list.append('hamster');
+list.append('snake');
+list.append('turtle');
+
+list.prepend('cheetah');
+
+console.table(list);
+console.table(list.at(0));
+
+console.log(list.toString());
+console.log(list.getSize());
+
+list.removeAt(6); // Should remove hamster
+
+console.log(list.toString());
+console.log(list.getSize());
